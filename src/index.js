6a63b8e7a1dcc4c4cf6ae70efea03046fed811c3
이재웅 <wodnd@jaeung-ui-MacBookPro.local>
2023-05-15 23:47:36 +0900

    westagram-react

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
-import "./styles/common.css"
+import "./styles/common.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
